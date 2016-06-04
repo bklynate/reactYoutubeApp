3977a2b41823331cfe6232e0123f59bdae85fa6b
bklynate <nathanielcfa@gmail.com>
2016-06-04 17:02:30 -0400

    change turn App into a class based component from a functional based one

--- src/index.js
+++ src/index.js
@@ -4,25 +4,27 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyD5IhvgM1SZ9iMr9arWOYIU35gSORLO3-U"
-// create a new component; it should produce HTML
-// const App = function() {
-//   return <div>Hello World</div>;
-// }
 
-// This code here is the same as the commented code above.
+class App extends Component {
+  constructor(props){
+    super(props);
 
-YTSearch({key:API_KEY, term:"50th law of power"}, function(data){
-  console.log(data);
-});
+    this.state = { videos: [] };
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+  // YTSearch({key:API_KEY, term:"50th law of power"}, function(videos){
+  //   this.setState({videos:videos})
+  // });
+    YTSearch({key:API_KEY, term:"50th law of power"}, (videos)=>{
+      this.setState({videos});
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
-
-// render generated HTML , and puts it into the DOM
-// aka "Show it on the page"
 ReactDOM.render(<App />, document.querySelector('.container'));
